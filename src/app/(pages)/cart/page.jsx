9924fa7fdfe73ff19e4a123162747fa7f0e37e8c
Harsh Gupta <59175412+harshgupta20@users.noreply.github.com>
2024-08-16 04:03:08 +0530

    cart remove item | update quantity of product

--- src/app/(pages)/cart/page.jsx
+++ src/app/(pages)/cart/page.jsx
@@ -1,51 +1,79 @@
-import CartProductCard from '@/components/CartProductCard'
-import React from 'react'
+"use client";
+
+import CartProductCard from '@/components/CartProductCard';
+import { ProductsContext } from '@/context/products';
+import { getProducts } from '@/utils/localstorage';
+import { useContext, useEffect, useState } from 'react';
 
 const CartPage = () => {
+
+  const {cartProducts} = useContext(ProductsContext);
+
   return (
     <>
       <div className='w-full h-screen flex bg-gray-100 p-4'>
         <div className='sm:w-8/12 p-6'>
           <div className='w-full p-4 shadow-md'>
-            <CartProductCard/>
+            {
+              cartProducts.length === 0 ? (
+                <div className='text-center'>
+                  <h1 className='text-2xl'>No products in cart</h1>
+                  <p className='text-gray-500'>Add products to cart to see them here</p>
+                </div>
+              ) : (
+                cartProducts.map((product, key) => <CartProductCard key={key} product={product}/>)
+              )
+            }
           </div>
         </div>
         <div className='sm:w-4/12 py-6'>
           <div className="max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-            <table className="w-full">
-              <thead className="bg-gray-100">
-                <tr>
-                  <th colSpan="2" className="text-left p-4 text-gray-600 font-medium">PRICE DETAILS</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr className="border-t">
-                  <td className="p-4 text-gray-600">Price (2 items)</td>
-                  <td className="p-4 text-right font-medium text-gray-800">₹13,989</td>
-                </tr>
-                <tr className="border-t">
-                  <td className="p-4 text-gray-600">Discount</td>
-                  <td className="p-4 text-right font-medium text-green-600">−₹10,192</td>
-                </tr>
-                <tr className="border-t">
-                  <td className="p-4 text-gray-600">Platform Fee</td>
-                  <td className="p-4 text-right font-medium text-gray-800">₹3</td>
-                </tr>
-                <tr className="border-t">
-                  <td className="p-4 text-gray-600">Delivery Charges</td>
-                  <td className="p-4 text-right font-medium text-gray-800"><s>₹150</s> <span className="text-green-600">Free</span></td>
-                </tr>
-                <tr className="border-t">
-                  <td className="p-4 text-gray-600 font-medium">Total Amount</td>
-                  <td className="p-4 text-right font-medium text-gray-800">₹3,800</td>
-                </tr>
-              </tbody>
-              <tfoot className="bg-green-50">
-                <tr className="border-t">
-                  <td colSpan="2" className="p-4 text-green-600 text-center font-medium">You will save ₹10,189 on this order</td>
-                </tr>
-              </tfoot>
-            </table>
+            {
+              cartProducts.length === 0 ? (
+                <>
+                  <div className="px-4 py-2">
+                    <h1 className="text-3xl text-gray-800 font-bold">Order Summary</h1>
+                    <p className="text-sm text-gray-600">No payable amount.</p>
+                  </div>
+                </>
+              )
+              : (
+                <table className="w-full">
+                  <thead className="bg-gray-100">
+                    <tr>
+                      <th colSpan="2" className="text-left p-4 text-gray-600 font-medium">PRICE DETAILS</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="border-t">
+                      <td className="p-4 text-gray-600">Price (2 items)</td>
+                      <td className="p-4 text-right font-medium text-gray-800">₹13,989</td>
+                    </tr>
+                    <tr className="border-t">
+                      <td className="p-4 text-gray-600">Discount</td>
+                      <td className="p-4 text-right font-medium text-green-600">−₹10,192</td>
+                    </tr>
+                    <tr className="border-t">
+                      <td className="p-4 text-gray-600">Platform Fee</td>
+                      <td className="p-4 text-right font-medium text-gray-800">₹3</td>
+                    </tr>
+                    <tr className="border-t">
+                      <td className="p-4 text-gray-600">Delivery Charges</td>
+                      <td className="p-4 text-right font-medium text-gray-800"><s>₹150</s> <span className="text-green-600">Free</span></td>
+                    </tr>
+                    <tr className="border-t">
+                      <td className="p-4 text-gray-600 font-medium">Total Amount</td>
+                      <td className="p-4 text-right font-medium text-gray-800">₹3,800</td>
+                    </tr>
+                  </tbody>
+                  <tfoot className="bg-green-50">
+                    <tr className="border-t">
+                      <td colSpan="2" className="p-4 text-green-600 text-center font-medium">You will save ₹10,189 on this order</td>
+                    </tr>
+                  </tfoot>
+                </table>
+              )
+            }
           </div>
 
         </div>
--- src/components/CartProductCard.jsx
+++ src/components/CartProductCard.jsx
@@ -1,36 +1,82 @@
-import Image from 'next/image'
-import React from 'react'
+"use client";
 
-const CartProductCard = () => {
+import { ProductsContext } from '@/context/products';
+import { errorAlert, successAlert } from '@/utils/alerts';
+import { deleteSingleProduct } from '@/utils/localstorage';
+import Image from 'next/image'
+import React, { useContext, useState } from 'react'
+
+const CartProductCard = ({product}) => {
+
+    const [loading, setLoading] = useState(false);
+    const {setCartProducts} = useContext(ProductsContext);
+
+    const handleRemoveFromCart = async () => {
+        try{
+            setLoading(true);
+            // simulate loading behavior
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+            const newProductsList = deleteSingleProduct(product);
+            successAlert(`${product?.title} removed from cart`);
+            setLoading(false);
+            setCartProducts(newProductsList);
+        }
+        catch(error){
+            errorAlert(error.message);
+            setLoading(false);
+        }
+    }
+
+    const handleCountChange = (countWithSign) => {
+        try{
+            const newProduct = {...product};
+            newProduct.quantity = (newProduct?.quantity ?? 1) + countWithSign;
+
+            if(newProduct.quantity > 10){
+                throw new Error("You can't add more than 10 items");
+            }
+            else if(newProduct.quantity < 1){
+                throw new Error("Quantity can't be less than 1");
+            }
+
+            setCartProducts((products) => products.map((product) => product.id === newProduct.id ? newProduct : product));
+            successAlert("Quantity updated");
+        }
+        catch(error){
+            errorAlert(error.message);
+        }
+    }
+    
+
   return (
     <div className='p-4'>
         <div className='flex justify-between'>
             <div className='flex gap-6'>
-                <Image src={"/assets/productImage.webp"} width={150} height={150} loading="lazy" alt='Product'/>
+                <Image src={product?.images[0]} width={150} height={150} loading="lazy" alt='Product'/>
                 <div className='flex flex-col justify-between py-2'>
                     <div>
-                        <h3 className='text-2xl'>Product Name</h3>
-                        <p className='text-sm text-gray-500'>Product Description</p>
+                        <h3 className='text-2xl'>{product?.title}</h3>
+                        <p className='text-sm text-gray-500'>{product?.description}</p>
                     </div>
-                    <div className='flex gap-2'>
-                        <p className='font-bold'>{2000} INR</p>
-                        <del className='text-gray-400'>{3000} INR</del>
-                        <p className='text-green-600 font-semibold'>{33} % off</p>
+                    <div className='flex flex-wrap gap-2'>
+                        <p className='font-bold'>{product?.price} INR</p>
+                        <del className='text-gray-400'>{((product?.price) + (product?.price*product?.discountPercentage)/100).toFixed(2) } INR</del>
+                        <p className='text-green-600 font-semibold'>{product.discountPercentage} % off</p>
                     </div>
                 </div>
             </div>
             <div>
-                <p>Delivered By 21 Auguest</p>
+                <p className='text-sm'>Delivered By 21 Auguest</p>
             </div>
         </div>
         <div className='flex place-items-center gap-3 justify-end'>
             <div className='flex place-items-center gap-3 border-2 border-gray-200 rounded-md'>
-                <button className='p-2 text-indigo-600 hover:bg-indigo-600 hover:text-white rounded-tl-md rounded-bl-md'>-</button>
-                <p className='text-sm'>20</p>
-                <button className='p-2 text-indigo-600 hover:bg-indigo-600 hover:text-white rounded-tr-md rounded-br-md'>+</button>
+                <button onClick={() => handleCountChange(-1)} className='p-2 text-indigo-600 hover:bg-indigo-600 hover:text-white rounded-tl-md rounded-bl-md'>-</button>
+                <p className='text-sm'>{product?.quantity ?? 1}</p>
+                <button onClick={() => handleCountChange(1)} className='p-2 text-indigo-600 hover:bg-indigo-600 hover:text-white rounded-tr-md rounded-br-md'>+</button>
             </div>
 
-            <button className='bg-red-600 text-white text-sm py-2 px-8 rounded-md'>Remove</button>
+            <button onClick={handleRemoveFromCart} disabled={loading} className='bg-red-600 text-white text-sm py-2 px-8 rounded-md'>{loading ? "Removing..." : "Remove"}</button>
         </div>
     </div>
   )
