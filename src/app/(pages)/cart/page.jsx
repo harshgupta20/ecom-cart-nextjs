9554a2ca4be7439f96a7ec8088d6cc1942b07888
Harsh Gupta <59175412+harshgupta20@users.noreply.github.com>
2024-08-16 23:25:23 +0530

    coupon code feature implemented

--- src/app/(pages)/cart/page.jsx
+++ src/app/(pages)/cart/page.jsx
@@ -2,6 +2,7 @@
 
 import CartProductCard from '@/components/CartProductCard';
 import { ProductsContext } from '@/context/products';
+import { errorAlert, successAlert } from '@/utils/alerts';
 import { deleteProducts } from '@/utils/localstorage';
 import { useContext, useEffect, useState } from 'react';
 import Swal from 'sweetalert2/dist/sweetalert2.js'
@@ -11,6 +12,7 @@ const CartPage = () => {
 
   const {cartProducts, setCartProducts} = useContext(ProductsContext);
   const [loading, setLoading] = useState(false);
+  const [isAdditional, setIsAdditional] = useState(false);
 
   const [priceDetails, setPriceDetails] = useState({
     totalAmount: 0,
@@ -62,6 +64,18 @@ const CartPage = () => {
     })
   }
 
+  async function applyCoupon(e) {
+    e.preventDefault();
+    if(e.target.coupon.value == "frontend"){
+      setPriceDetails((priceDetails) => {return {...priceDetails, totalPayableAmount: priceDetails.totalPayableAmount - (priceDetails.totalPayableAmount*0.1)}});
+      successAlert("Coupon Applied ✨");
+      setIsAdditional(true);
+    }
+    else{
+      errorAlert("Invalid Coupon Code");
+    }
+  }
+
   useEffect(() => {
     calculateTotalPricing();
   }, [cartProducts]);
@@ -91,7 +105,7 @@ const CartPage = () => {
                   <table className="w-full">
                   <thead className="bg-gray-100">
                     <tr>
-                      <th colSpan="2" className="text-left p-4 text-gray-600 font-medium">PRICE DETAILS</th>
+                      <th colSpan="2" className="text-left p-4 text-gray-600 font-medium">PRICE DETAILS (Coupon Code : frontend)</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -111,6 +125,12 @@ const CartPage = () => {
                       <td className="p-4 text-gray-600">Delivery Charges</td>
                       <td className="p-4 text-right font-medium text-gray-800"><s>{priceDetails.deliveryCharges} INR</s> <span className="text-green-600">Free</span></td>
                     </tr>
+                    {isAdditional && (
+                        <tr className="border-t">
+                          <td className="p-4 text-gray-600">Additional Discount</td>
+                          <td className="p-4 text-right font-medium text-green-600">10% off</td>
+                        </tr>
+                    )}
                     <tr className="border-t">
                       <td className="p-4 text-gray-600 font-medium">Total Amount</td>
                       <td className="p-4 text-right font-medium text-gray-800">{priceDetails.totalPayableAmount.toFixed(2)} INR</td>
@@ -121,6 +141,20 @@ const CartPage = () => {
                       <td colSpan="2" className="p-4 text-green-600 text-center font-medium">You will save {priceDetails.totalSaving.toFixed(2)} INR on this order</td>
                     </tr>
                   </tfoot>
+                  {
+                    !isAdditional && (
+                        <tfoot className="bg-green-50">
+                          <tr className="border-t">
+                            <td colSpan="2" >
+                                <form className='w-full grid grid-cols-2' onSubmit={applyCoupon}>
+                                  <input type="text" required name="coupon" placeholder="Enter Coupon Code" className="p-4 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-600" />
+                                  <button type='submit' className="p-4 bg-indigo-600 text-white cursor-pointer text-center font-medium w-full hover:opacity-80">{loading ? "Applying..." : "Apply coupon"}</button>
+                                </form>
+                            </td>
+                          </tr>
+                        </tfoot>
+                    )
+                  }
                   <tfoot className="bg-green-50">
                     <tr className="border-t">
                       
